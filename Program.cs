4cc2a74c5768ae976f919d01b199fe6150d3c9d4
Jonathan Falanga <BookGeekRR@outlook.com>
2021-06-09 17:28:01 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> MyList = new List<string>();
+            MyList.Add("John");
+            MyList.Add("Jacob");
+            MyList.Add("Jingle");
+            MyList.Add("Himer");
+            MyList.Add("Schmitt");
+            //MyList.Add("John");
+            //MyList.Add("Jacob");
+
+            foreach (string jostar in MyList)
+            {
+                Console.WriteLine(jostar);
+            }
+
+            Console.WriteLine("The count: " + MyList.Count);
+            Console.WriteLine("Capacity thingy (for MyList!): " + MyList.Capacity);
+
+            Console.WriteLine("Trimming...");
+            MyList.TrimExcess();
+            Console.WriteLine("The count: " + MyList.Count);
+            Console.WriteLine("Capacity thingy (for MyList!): " + MyList.Capacity);
+            Console.WriteLine();
+
+
+        }
+    }
+}
